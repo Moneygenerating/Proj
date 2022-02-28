0e083dab3098f83bfee5c874dd7bd2168d39bc75
Martin <septum1@yandex.ru>
2022-02-28 21:49:25 +0700

    Обьявил метод main

--- Praktikum.java
+++ Praktikum.java
@@ -2,12 +2,6 @@ import java.util.HashMap;
 
 public class Praktikum {
     public static void main(String[] args) {
-        HashMap<String, String> officeTool = new HashMap<>();
-        officeTool.put("S234", "Большой степлер");
-        officeTool.put("P342", "Чёрно-белый принтер");
-        officeTool.put("N845", "Острые ножницы");
-
-        String tool = officeTool.get("N845"); 
-        System.out.println(tool); // Получили "Острые ножницы"
+        
     }
 }
