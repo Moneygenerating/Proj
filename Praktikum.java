79b2104f06d62b149172714a9542cb6f86a37037
Martin <septum1@yandex.ru>
2022-02-28 21:46:59 +0700

    My first commit on Eng!

---
+++ Praktikum.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        HashMap<String, String> officeTool = new HashMap<>();
+        officeTool.put("S234", "Большой степлер");
+        officeTool.put("P342", "Чёрно-белый принтер");
+        officeTool.put("N845", "Острые ножницы");
+
+        String tool = officeTool.get("N845"); 
+        System.out.println(tool); // Получили "Острые ножницы"
+    }
+}
